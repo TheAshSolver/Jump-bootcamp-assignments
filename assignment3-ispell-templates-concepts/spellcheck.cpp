b47db43872ae016e05d3ab1e171248a51d059889
sed9871 <dashwinaravind1@gmail.com>
2026-05-30 00:44:43 +0530

    first part of spell check completed

--- assignment3-ispell-templates-concepts/spellcheck.cpp
+++ assignment3-ispell-templates-concepts/spellcheck.cpp
@@ -6,13 +6,19 @@
 #include <ranges>
 #include <set>
 #include <vector>
+#include<cctype>
 
 template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
-  /* TODO: Implement this method */
-  return Corpus();
+  std::vector<std::string::iterator> x  = find_all(source.begin(), source.end(), [](auto &x){return std::isspace(static_cast<unsigned char >(x));});
+  Corpus my_corpus;
+  std::transform(x.begin(), x.end()-1, x.begin()+1, std::inserter(my_corpus, my_corpus.end()), [&source](auto& x1, auto& x2){return Token(source, x1+1, x2);});
+  std::erase_if(my_corpus, [](const Token &token){
+    return token.content.empty();
+  });
+  return my_corpus;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
