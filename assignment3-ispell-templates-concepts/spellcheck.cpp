a068cd4b582271e0bf7a1c01e15cb6be6b6e88e5
sed9871 <dashwinaravind1@gmail.com>
2026-05-30 01:11:21 +0530

    completed spell check

--- assignment3-ispell-templates-concepts/spellcheck.cpp
+++ assignment3-ispell-templates-concepts/spellcheck.cpp
@@ -14,7 +14,7 @@ std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 Corpus tokenize(std::string& source) {
   std::vector<std::string::iterator> x  = find_all(source.begin(), source.end(), [](auto &x){return std::isspace(static_cast<unsigned char >(x));});
   Corpus my_corpus;
-  std::transform(x.begin(), x.end()-1, x.begin()+1, std::inserter(my_corpus, my_corpus.end()), [&source](auto& x1, auto& x2){return Token(source, x1+1, x2);});
+  std::transform(x.begin(), x.end()-1, x.begin()+1, std::inserter(my_corpus, my_corpus.end()), [&source](auto& x1, auto& x2){return Token(source, x1, x2);});
   std::erase_if(my_corpus, [](const Token &token){
     return token.content.empty();
   });
@@ -22,8 +22,21 @@ Corpus tokenize(std::string& source) {
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
-  /* TODO: Implement this method */
-  return std::set<Misspelling>();
+  auto view = source  | std::ranges::views::filter([&dictionary](const Token &token){return !dictionary.contains(token.content);})
+                      | std::ranges::views::transform([&dictionary](const Token &token){
+                      std::set<std::string> strings;
+                      std::for_each(dictionary.begin(), dictionary.end(), [ &token,  &strings](const std::string &word){
+                        if(levenshtein(word, token.content)==1){
+                          strings.insert(word);
+                        }
+                      });
+                     return Misspelling{token, strings};
+                    })
+                    | std::ranges::views::filter([](const Misspelling &mispelling){
+                      return !mispelling.suggestions.empty();
+                    });
+  return std::set<Misspelling>(view.begin(), view.end());
+
 };
 
 /* Helper methods */
